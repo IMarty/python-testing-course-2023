c31a8175be97ebe98483140492294101b878a868
IMarty <igor.marty@telecomnancy.net>
2024-01-03 12:04:30 +0100

    Exercice

--- tests/day_2/conftest.py
+++ tests/day_2/conftest.py
@@ -9,3 +9,6 @@ def restaurant_reviews_with_two_restaurants():
     rr.add_review("Cafe Mocha", "Great Coffe.", 4)
     rr.add_review("Cafe Burger", "Good Burger", 3)
     return rr
+
+# Cours : nous avons vu ensemble comment utiliser les fixture dans le test_fixture_review_get.py
+# Exercice : Créer et écrire les tests add/update/delete en utilisant le/la fixture
